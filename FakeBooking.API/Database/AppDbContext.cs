0e299667e0cdf3e3d4fe8c15906d60bb1ee09c86
Pineapple <xcchenmichael@gmail.com>
2025-05-30 18:34:08 -0400

    Test data insertion into database

--- FakeBooking.API/Database/AppDbContext.cs
+++ FakeBooking.API/Database/AppDbContext.cs
@@ -1,5 +1,8 @@
 ﻿using FakeBooking.API.Models;
 using Microsoft.EntityFrameworkCore;
+using Newtonsoft.Json;
+using System.Reflection;
+
 
 
 namespace FakeBooking.API.Database
@@ -13,5 +16,27 @@ namespace FakeBooking.API.Database
 
         public DbSet<TouristRoute> TouristRoutes { get; set; }
         public DbSet<TouristRoutePicture> TouristRoutePictures { get; set; }
+
+        protected override void OnModelCreating(ModelBuilder modelBuilder)
+        {
+            //modelBuilder.Entity<TouristRoute>().HasData(new TouristRoute()
+            //{
+            //    Id = Guid.NewGuid(),
+            //    Title = "TestTitle",
+            //    Description = "TestDescription",
+            //    OriginalPrice = 0,
+            //    CreateTime = DateTime.UtcNow
+            //});
+
+            var touristRouteJsonData = File.ReadAllText(Path.GetDirectoryName(Assembly.GetExecutingAssembly().Location) + @"/Database/touristRoutesMockData.json");
+            IList<TouristRoute> touristRoutes = JsonConvert.DeserializeObject<IList<TouristRoute>>(touristRouteJsonData);
+            modelBuilder.Entity<TouristRoute>().HasData(touristRoutes);
+
+            var touristRoutePictureJsonData = File.ReadAllText(Path.GetDirectoryName(Assembly.GetExecutingAssembly().Location) + @"/Database/touristRoutePicturesMockData.json");
+            IList<TouristRoutePicture> touristRoutePictures = JsonConvert.DeserializeObject<IList<TouristRoutePicture>>(touristRoutePictureJsonData);
+            modelBuilder.Entity<TouristRoutePicture>().HasData(touristRoutePictures);
+
+            base.OnModelCreating(modelBuilder);
+        }
     }
 }
---
+++ FakeBooking.API/Migrations/20250530215258_DataSeeding.cs
@@ -0,0 +1,47 @@
+﻿using System;
+using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace FakeBooking.API.Migrations
+{
+    /// <inheritdoc />
+    public partial class DataSeeding : Migration
+    {
+        /// <inheritdoc />
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.AlterColumn<DateTime>(
+                name: "CreateTime",
+                table: "TouristRoutes",
+                type: "TEXT",
+                nullable: false,
+                defaultValueSql: "GETDATE()",
+                oldClrType: typeof(DateTime),
+                oldType: "TEXT");
+
+            migrationBuilder.InsertData(
+                table: "TouristRoutes",
+                columns: new[] { "Id", "CreateTime", "DepartureTime", "Description", "DiscountPresent", "Features", "Fees", "Notes", "OriginalPrice", "Title", "UpdateTime" },
+                values: new object[] { new Guid("9a5f7c31-67fb-435a-8fba-c85617aa34e1"), new DateTime(2025, 5, 30, 21, 52, 57, 587, DateTimeKind.Utc).AddTicks(1193), null, "TestDescription", null, null, null, null, 0m, "TestTitle", null });
+        }
+
+        /// <inheritdoc />
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DeleteData(
+                table: "TouristRoutes",
+                keyColumn: "Id",
+                keyValue: new Guid("9a5f7c31-67fb-435a-8fba-c85617aa34e1"));
+
+            migrationBuilder.AlterColumn<DateTime>(
+                name: "CreateTime",
+                table: "TouristRoutes",
+                type: "TEXT",
+                nullable: false,
+                oldClrType: typeof(DateTime),
+                oldType: "TEXT",
+                oldDefaultValueSql: "GETDATE()");
+        }
+    }
+}
